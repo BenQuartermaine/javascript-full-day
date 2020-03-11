56e233601432d07ff7f786e7ac3ccc71a47c1025
BenQuartermaine <ben.quartermaine3@gmail.com>
2020-03-11 19:01:37 +1100

    style and clean up changes

--- main.js
+++ main.js
@@ -1,38 +1,21 @@
-console.log(movieList)
 const list = document.querySelector('#results');
 
-
-// const renderMovies = (movies) => {
-//   movies.forEach((movie) => {
-//     const movieItem = `<div class="card">
-//       <div class="bkg" style="background-image: url(${movie.image})"></div>
-//       <p>${movie.title}</p>
-//     </div>`;
-//     list.insertAdjacentHTML('beforeend', movieItem);
-//   });
-// };
-
-// API Call InsertMovies
 const insertMovies = (data) => {
   console.log(data)
   data.Search.forEach((result) => {
     const movie = `<div class="card">
-          <img class="bkg" src="${result.Poster}" />
+          <img src="${result.Poster}" />
           <p>${result.Title}</p>
       </div>`;
     list.insertAdjacentHTML('beforeend', movie);
   });
 };
 
-
-// API Call
 const fetchMovies = (query) => {
   fetch(`https://www.omdbapi.com/?s=${query}&apikey=adf1f2d7`)
     .then(response => response.json())
     .then(insertMovies);
 };
-
-// renderMovies(movieList)
 
 fetchMovies('harry potter'); // on 1st page load
 
